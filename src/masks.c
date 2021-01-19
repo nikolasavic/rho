08c053b2a9fe21f835a43f3f14ee36bc346c68cd
Nikola Savic <nikolasavic@gmail.com>
2021-01-18 21:52:06 -0500

    Add knight attack edge and corner cases

--- src/masks.c
+++ src/masks.c
@@ -2,12 +2,16 @@
 #include "bitboard.h"
 
 const U64 FILE_A = 72340172838076673ULL;
+const U64 FILE_AB = 217020518514230019ULL;
+const U64 FILE_GH = 13889313184910721216ULL;
 const U64 FILE_H = 9259542123273814144ULL;
+const U64 RANK_12 = 18446462598732840960ULL;
 const U64 RANK_1 = 18374686479671623680ULL;
 const U64 RANK_2 = 71776119061217280ULL;
 const U64 RANK_7 = 65280ULL;
 const U64 RANK_8 = 255ULL;
 const U64 RANK_18 = 18374686479671623935ULL;
+const U64 RANK_78 = 65535ULL;
 
 U64 pawn_attack_mask(square, side) {
   U64 bb = 0ULL;
@@ -86,13 +90,21 @@ U64 king_attack_mask(square) {
 
 U64 knight_attack_mask(square) {
   U64 bb = 0ULL;
-  set_bit((square - 15), bb);
-  set_bit((square - 17), bb);
-  set_bit((square - 6), bb);
-  set_bit((square - 10), bb);
-  set_bit((square + 6), bb);
-  set_bit((square + 10), bb);
-  set_bit((square + 15), bb);
-  set_bit((square + 17), bb);
+  if(!get_bit(square, RANK_78) && !get_bit(square, FILE_H))
+    set_bit((square - 15), bb);
+  if(!get_bit(square, FILE_A) && !get_bit(square, RANK_78))
+    set_bit((square - 17), bb);
+  if(!get_bit(square, RANK_8) && !get_bit(square, FILE_GH))
+    set_bit((square - 6), bb);
+  if(!get_bit(square, FILE_AB) && !get_bit(square, RANK_8))
+    set_bit((square - 10), bb);
+  if(!get_bit(square, RANK_1) && !get_bit(square, FILE_AB))
+    set_bit((square + 6), bb);
+  if(!get_bit(square, RANK_1) && !get_bit(square, FILE_GH))
+    set_bit((square + 10), bb);
+  if(!get_bit(square, RANK_12) && !get_bit(square, FILE_A))
+    set_bit((square + 15), bb);
+  if(!get_bit(square, RANK_12) && !get_bit(square, FILE_H))
+    set_bit((square + 17), bb);
   return bb;
 }
--- test/test_masks.c
+++ test/test_masks.c
@@ -177,8 +177,112 @@ void test_knight_attack_mask(void) {
   set_bit(D1, expected);
   set_bit(E2, expected);
   set_bit(E4, expected);
-  print_bitboard(knight_attack_mask(C3));
   TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(C3));
+
+  expected = 0ULL;
+  set_bit(A1, expected);
+  set_bit(A3, expected);
+  set_bit(B4, expected);
+  set_bit(D4, expected);
+  set_bit(E1, expected);
+  set_bit(E3, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(C2));
+
+  expected = 0ULL;
+  set_bit(A2, expected);
+  set_bit(B3, expected);
+  set_bit(D3, expected);
+  set_bit(E2, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(C1));
+
+  expected = 0ULL;
+  set_bit(A3, expected);
+  set_bit(C3, expected);
+  set_bit(D2, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(B1));
+
+  expected = 0ULL;
+  set_bit(A4, expected);
+  set_bit(C4, expected);
+  set_bit(D3, expected);
+  set_bit(D1, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(B2));
+
+  expected = 0ULL;
+  set_bit(B3, expected);
+  set_bit(C2, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(A1));
+
+  expected = 0ULL;
+  set_bit(B4, expected);
+  set_bit(C3, expected);
+  set_bit(C1, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(A2));
+
+  expected = 0ULL;
+  set_bit(B5, expected);
+  set_bit(B1, expected);
+  set_bit(C4, expected);
+  set_bit(C2, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(A3));
+
+  expected = 0ULL;
+  set_bit(B5, expected);
+  set_bit(C8, expected);
+  set_bit(C6, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(A7));
+
+  expected = 0ULL;
+  set_bit(B6, expected);
+  set_bit(C7, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(A8));
+
+  expected = 0ULL;
+  set_bit(A6, expected);
+  set_bit(C6, expected);
+  set_bit(D7, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(B8));
+
+  expected = 0ULL;
+  set_bit(F6, expected);
+  set_bit(G7, expected);
+  set_bit(C7, expected);
+  set_bit(D6, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(E8));
+
+  expected = 0ULL;
+  set_bit(C8, expected);
+  set_bit(G8, expected);
+  set_bit(F5, expected);
+  set_bit(G6, expected);
+  set_bit(C6, expected);
+  set_bit(D5, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(E7));
+
+  expected = 0ULL;
+  set_bit(E8, expected);
+  set_bit(E6, expected);
+  set_bit(F5, expected);
+  set_bit(H5, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(G7));
+
+  expected = 0ULL;
+  set_bit(F7, expected);
+  set_bit(G6, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(H8));
+
+  expected = 0ULL;
+  set_bit(F8, expected);
+  set_bit(F6, expected);
+  set_bit(G5, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(H7));
+
+  expected = 0ULL;
+  set_bit(G8, expected);
+  set_bit(F7, expected);
+  set_bit(F5, expected);
+  set_bit(G4, expected);
+  TEST_ASSERT_EQUAL_INT(expected, knight_attack_mask(H6));
 }
 
 int main(void) {
