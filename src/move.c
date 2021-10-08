c1f591636a715634ad01114550644d9a81eebcc0
Nikola Savic <nikolasavic@fastmail.com>
2021-10-08 15:38:49 -0400

    Add simple move validation

--- src/move.c
+++ src/move.c
@@ -1,8 +1,38 @@
+#include <stdio.h>
 #include "move.h"
 #include "exits.h"
 
 exit_t validate_move(move_t *move, val_opt_t option) {
-  return FAIL;
+  if(move->origin < 0 || move->origin > 63) {
+    if(option == VERBOSE) {
+      printf("Error: invalid origin square: %d\n", move->origin);
+    }
+    return FAIL;
+  }
+
+  if(move->target < 0 || move->target > 63) {
+    if(option == VERBOSE) {
+      printf("Error: invalid target square: %d\n", move->target);
+    }
+    return FAIL;
+  }
+
+  if(move->promotion < 1 || move->promotion > 6) {
+    if(option == VERBOSE) {
+      printf("Error: invalid promotion piece: %d\n", move->promotion);
+    }
+    return FAIL;
+  }
+
+  if(move->capture &&
+      (move->quiet_move || move->double_pawn || move->kingside_castle || move-> queenside_castle)) {
+    if(option == VERBOSE) {
+      printf("Error: capture flag on a quiet move: %d\n", move->promotion);
+    }
+    return FAIL;
+  }
+
+  return SUCCESS;
 }
 
 U16 encode_move(move_t *move) {
--- test/test_move.c
+++ test/test_move.c
@@ -7,7 +7,23 @@ void setUp(void) {
 void tearDown(void) {
 }
 
+void test_validate_move(void) {
+  move_t move;
+  move.origin = E2;
+  move.target = E4;
+  move.promotion = NULL_P;
+  move.quiet_move = true;
+  move.double_pawn = true;
+  move.capture = false;
+  move.ep_capture = false;
+  move.kingside_castle = false;
+  move.queenside_castle = false;
+
+  TEST_ASSERT_EQUAL_INT(SUCCESS, validate_move(&move, SILENT));
+}
+
 int main(void) {
   UNITY_BEGIN();
+  RUN_TEST(test_validate_move);
   return UNITY_END();
 }
