8285f7e132d7a5c558d0ca285cdc9a7b9e494be6
Nikola Savic <nikolasavic@fastmail.com>
2021-10-14 21:44:36 -0400

    Fix formatter to work on MacOS

--- format.sh
+++ format.sh
@@ -1,2 +1,7 @@
-indent src/* test/*
-rm src/*~ test/*~
+if [ "$(uname -s)" == "Darwin" ]; then
+  gindent src/* test/*
+  rm src/*~ test/*~
+elif [ "$(uname -s)" == "Linux" ]; then
+  indent src/* test/*
+  rm src/*~ test/*~
+fi
