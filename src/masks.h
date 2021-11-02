6e8986a9d189d0445ce54feb75613ecc0514f0d9
Nikola Savic <nikolasavic@fastmail.com>
2021-11-02 11:03:24 -0400

    Remove rook & bishop attack masks
    Switching over from magic bitboard implementation to "classical
    approach"

--- src/masks.h
+++ src/masks.h
@@ -7,8 +7,6 @@ U64 pawn_attack_mask(square_t, side_t);
 U64 pawn_motion_mask(square_t, side_t);
 U64 king_attack_mask(square_t);
 U64 knight_attack_mask(square_t);
-U64 bishop_attack_mask(square_t, U64);
-U64 rook_attack_mask(square_t, U64);
 U64 ray_north(square_t);
 U64 ray_north_east(square_t);
 U64 ray_east(square_t);
